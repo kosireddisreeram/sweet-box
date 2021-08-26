efcc97135f5acb5a9f282391b08adc5eaa743d01
sreeram <kosireddisreeram@gmail.com>
2021-08-26 12:57:55 +0530

    Initial commit

---
+++ javascript/one.js
@@ -0,0 +1,70 @@
+// // console.log(typeof("boolean"));
+// // console.log(typeof(1));
+// // console.log(typeof(true));
+// // var year=1991;
+// // console.log(typeof(year));
+// // const now=2037;
+// // const jonaBirthyear=now-1991;
+// // console.log(jonaBirthyear);
+// // const rBirthyear=now-1998;
+// // console.log(rBirthyear);
+// // console.log(jonaBirthyear*2,jonaBirthyear-2,rBirthyear*2,rBirthyear-2);
+// // const firstName="jonas";
+// // const lastName="schemed";
+// // console.log( firstName + '' + lastName);
+// // console.log(jonaBirthyear>rBirthyear);
+// // console.log(rBirthyear>jonaBirthyear);
+
+
+// // CODING CHALLENGE-1
+// // var weightofmark=78;
+// // var heightofmark=1.69;
+// // var weightofjohn=92;
+// // var heightofjohn=1.95;
+// // var bmiofmark=(weightofmark/(heightofmark*heightofmark));
+// // var bmiofjohn=(weightofjohn/(heightofjohn*heightofjohn));
+// // console.log(bmiofjohn);
+// // console.log(bmiofmark);
+// // console.log(bmiofmark>bmiofjohn);
+// // console.log(bmiofjohn>bmiofmark);
+
+
+
+
+// // CODING CHALLENGE -2
+//   var weightofmark=95;
+//   var heightofmark=1.88;
+//   var weightofjohn=85;
+//   var heightofjohn=1.76;
+//   var bmiofjohn=(weightofjohn/(heightofjohn*heightofjohn));
+//   var bmiofmark=(weightofmark/(heightofmark*heightofmark));
+//   console.log(bmiofjohn);
+//   console.log(bmiofmark);
+//   console.log(bmiofmark>bmiofjohn);
+//   console.log(bmiofjohn>bmiofmark);
+//   console.log(bmiofjohn +'' +"is greater than the "+ ''+ bmiofmark);
+// console.log(`weight of john is ${weightofjohn}`);
+// console.log(`weight of mark is  ${weightofmark}`);
+
+
+//if else 
+
+// var age = 15;
+// // const isOldenough=age>=18;
+// if(age>=18){
+//     console.log(`he can have the driving license`);
+// }
+// else{
+//     console.log(`he should wait for sometime`);
+// }
+
+let century;
+const Birthyear=1921;
+if(Birthyear>=2000){
+
+     century=21;
+}
+else{
+     century=20;
+}
+console.log(century);
